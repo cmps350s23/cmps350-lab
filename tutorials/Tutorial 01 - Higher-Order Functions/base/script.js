2987eed3ed6fbf7095141a5f2357e0bc0795730b
Georges <georges.younes@gmail.com>
2023-04-20 16:51:07 +0300

    add document for tut01

---
+++ tutorials/Tutorial 01 - Higher-Order Functions/base/script.js
@@ -0,0 +1,15 @@
+const array = range(1, 20);
+
+console.log("Max:", max(7, 1, 2, -3));
+console.log("Max:", max(1, 5, -10, 23, 0, -7, 18));
+console.log("Range 1 to 10:", range(1, 10));
+console.log("Random 20, 1 to 100:", randoms(20, 1, 100));
+console.log("Factorial 0:", factorial(0));
+console.log("Factorial 5:", factorial(5));
+console.log("Divisors 1:", divisors(7));
+console.log("Divisors 12:", divisors(12));
+console.log("Prime 1:", isPrime(1));
+console.log("Prime 23:", isPrime(23));
+console.log("PrimeProduct 1 to 100:", primeProduct(1, 100));
+console.log("Tally 1 to 20:", tally(array));
+console.log("Reverse 1 to 20:", reverse(array));
